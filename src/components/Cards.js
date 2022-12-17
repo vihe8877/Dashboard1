613b4d90113361f82abd62fbbf4d0d5fdb424c6b
vihe8877 <88011859+vihe8877@users.noreply.github.com>
2022-12-16 20:26:02 -0500

    Cleaned up and made it for Grillic

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,25 +5,24 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Leftovers Near You:</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
+              src='public\images\apples.jpg'
+              text='Nice and crispy apples'
+              path='/products'
             />
-            <CardItem
+            {/* <CardItem
               src='images/img-2.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
-            />
+            /> */}
           </ul>
           <ul className='cards__items'>
-            <CardItem
+            {/* <CardItem
               src='images/img-3.jpg'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
@@ -40,7 +39,7 @@ function Cards() {
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
-            />
+            /> */}
           </ul>
         </div>
       </div>
